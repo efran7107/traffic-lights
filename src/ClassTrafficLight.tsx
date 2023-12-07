386f14415a68d8e135a4a02a760c471f16d8bcd3
ernest <efran7107@gmail.com>
2023-12-06 19:40:20 -0700

    finished

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,42 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  lights: Array<string> = ["black", "green", "yellow", "red"];
+
+  state = {
+    light: 1,
+  };
+
+  changeLight: React.MouseEventHandler<HTMLButtonElement> = () => {
+    if (this.state.light === 3) this.setState({ light: 1 });
+    else this.setState({ light: this.state.light + 1 });
+  };
+
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div
+            className={`circle ${
+              this.state.light === 3 ? this.lights[3] : this.lights[0]
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.light === 2 ? this.lights[2] : this.lights[0]
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.light === 1 ? this.lights[1] : this.lights[0]
+            }`}
+          ></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.changeLight}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
+const lights: Array<string> = ["black", "green", "yellow", "red"];
+
 export const FunctionalTrafficLight = () => {
+  const [light, switchLight] = useState(1);
+
+  const changeLight: React.MouseEventHandler<HTMLButtonElement> = () => {
+    if (light === 3) switchLight(1);
+    else switchLight(light + 1);
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${light === 3 ? lights[3] : lights[0]}`}></div>
+        <div className={`circle ${light === 2 ? lights[2] : lights[0]}`}></div>
+        <div className={`circle ${light === 1 ? lights[1] : lights[0]}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={changeLight}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
